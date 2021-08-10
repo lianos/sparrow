db7049a386c827f98a165b3759162643d5357233
lianos <lianoglou@dnli.com>
2021-08-10 14:20:43 -0700

    adds shiny dependencies to base images

--- inst/docker/devel/install.R
+++ inst/docker/devel/install.R
@@ -2,5 +2,5 @@ installed.pkgs <- rownames(installed.packages())
 required.pkgs <- readLines("/tmp/packages.txt")
 install.me <- setdiff(required.pkgs, installed.pkgs)
 
+# Trigger workflow, please.
 BiocManager::install(install.me, update = FALSE, ask = FALSE)
-
